f0015241c930a054347274a5dc012511012e8ced
Connor Boyle <cjblink1@gmail.com>
2018-10-11 22:39:37 -0400

    Blue now

--- src/app/model/utilities.ts
+++ src/app/model/utilities.ts
@@ -1,7 +1,12 @@
 
+const cold = [0 / 255, 0 / 255, 60 / 255];
+const hot = [235 / 255, 245 / 255, 251 / 255];
+const slopes = hot.map((_, i) => (hot[i] - cold[i]) / 255);
+
+
 export function tempToColor(temp: number): Array<number> {
-  const color = Math.round(temp) / 256;
-  return [color, color, color, 1.0];
+  const result = slopes.map((slope, i) => cold[i] + slope * temp).concat([1.0]);
+  return result;
 }
 
 export function executeAndMeasure(name: string, f: () => void, obj): number {
